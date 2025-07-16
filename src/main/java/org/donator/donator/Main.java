c52a38156c31533e7ae33980a60688572d11cb72
ComaTV <comatvc@gmail.com>
2025-07-16 09:48:33 +0300

    try

--- src/main/java/org/donator/donator/Main.java
+++ src/main/java/org/donator/donator/Main.java
@@ -1,6 +1,7 @@
 package org.donator.donator;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import io.papermc.paper.command.CommandManager;
 
 public final class Main extends JavaPlugin {
 
@@ -9,9 +10,10 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         donatorManager = new DonatorManager(this);
-        this.getCommand("donator").setExecutor(new DonatorCommand(this, donatorManager));
-        this.getCommand("investor").setExecutor(new InvestorCommand(donatorManager));
-        this.getCommand("car").setExecutor(new CarCommand(this, donatorManager));
+        CommandManager cm = this.getServer().getCommandManager();
+        cm.register("donator", new DonatorCommand(this, donatorManager));
+        cm.register("investor", new InvestorCommand(donatorManager));
+        cm.register("car", new CarCommand(this, donatorManager));
         getServer().getPluginManager().registerEvents(new DonatorListener(), this);
     }
 
